9221f9785ea05a2282d8f152fc9f7ecda80da127
Gabriel Santana Oliveira de Paula <50323251+Gabsop@users.noreply.github.com>
2020-06-16 13:23:25 -0300

    Update aula02.cpp

--- aula02.cpp
+++ aula02.cpp
@@ -2,21 +2,20 @@
 #include <string>
 
 int main(int argc, char const *argv[]) {
-  std::string const function = "add";
+  std::string function = "add";
+  std::string mensagem;
 
-  if (argc == 1 || function.compare(argv[1]) != 0) {
-    std::cout << "Uso: " << argv[0] << " " << function << " <mensagem>\n";
-    return -1;
+  if (argc == 1) {
+        std::cout << "Uso: " << argv[0] << " " << function << " <mensagem>\n";
+        return -1;
   }
-
-   std::string mensagem;
-
-   if (argc == 2) {
+  
+   if (argc == 2 && string(argv[1]) == function) {
      std::cout << "Digite uma mensagem:\n";
      std::getline(std::cin, mensagem);
    }
 
-   if (argc > 2) {
+   if (argc > 2 && string(argv[1]) == function) {
        for (int i = 2; i < argc; i++){
             mensagem += argv[i];
             mensagem += " ";
