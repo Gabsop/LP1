fa4a752e6c009e60c4e43b3fd6bbf3c3a300ef57
Gabriel Santana Oliveira de Paula <50323251+Gabsop@users.noreply.github.com>
2020-06-16 12:59:00 -0300

    Add files via upload

---
+++ aula02.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char const *argv[]) {
+  std::string const function = "add";
+
+  if (argc == 1 || function.compare(argv[1]) != 0) {
+    std::cout << "Uso: " << argv[0] << " " << function << " <mensagem>\n";
+    return -1;
+  }
+
+   std::string mensagem;
+
+   if (argc == 2 || function.compare(argv[1]) != 0) {
+     std::cout << "Digite uma mensagem:\n";
+     std::getline(std::cin, mensagem);
+   }
+
+   if (argc > 2 || function.compare(argv[1]) != 0) {
+       for (int i = 2; i < argc; i++){
+            mensagem += argv[i];
+            mensagem += " ";
+       }
+   }
+
+   std::cout << "Mensagem adicionada!\n" << mensagem << "\n";
+
+   return 0;
+}
