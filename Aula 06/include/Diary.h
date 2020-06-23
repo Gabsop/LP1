4bcf58a8fec24c8cba67240a963e7c48d56ceaaf
Gabsop <gabsop@hotmail.com>
2020-06-23 19:04:25 -0300

    Aula 06

---
+++ Aula 06/include/Diary.h
@@ -0,0 +1,20 @@
+#ifndef DIARY_H
+#define DIARY_H
+
+#include "Message.h"
+
+#include <string>
+
+struct Diary
+{
+    Diary(const std::string& filename);
+
+    std::string filename;
+    Message* messages;
+    size_t messages_size; // Quantas messages realmente tem
+    size_t messages_capacity; // Máximo que o array suporta
+
+    void add(const std::string& message); // Adicionar mais de uma mensagem por vez utilizando o write()
+    void write(); // Escreve as mensagens no arquivo
+};
+#endif
---
+++ Aula 06/src/Diary.cpp
@@ -0,0 +1,16 @@
+#include "../include/Diary.h"
+
+Diary::Diary(const std::string& filename): filename(filename)
+{
+
+}
+
+void Diary::add(const std::string& message)
+{
+    //Adicionar mensagem no array de mensagens
+}
+
+void Diary::write()
+{
+    // Gravar as mensagens no disco
+}
